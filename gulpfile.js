798be15bcc43ecae8a862657c540865cecac387e
ifozest <aemaeth4@gmail.com>
2014-07-13 22:57:17 +0300

    added few gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,47 @@
+var gulp = require('gulp')
+  , changed = require('gulp-changed')
+  , minifyHTML = require('gulp-minify-html')
+  , rimraf = require('gulp-rimraf')
+  , source = require('vinyl-source-stream')
+  , browserify = require('gulp-browserify')
+  , jshint = require('gulp-jshint');
+
+
+gulp.task('jshint', function () {
+  gulp.src('./src/js/**/*.js', {lookup: false})
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('jshint-stylish'))
+    .pipe(jshint.reporter('fail'))
+    .on('error', function (err) {
+      console.error(err.message);
+      this.emit('end');
+    });
+});
+
+gulp.task('minify-html', function () {
+  var htmlSrc = './src/**/*.html'
+    , htmlDest = './public';
+
+  gulp.src(htmlSrc)
+    .pipe(changed(htmlDest))
+    .pipe(minifyHTML())
+    .pipe(gulp.dest(htmlDest));
+});
+
+/**
+ * removes public folder
+ */
+gulp.task('clean', function () {
+  gulp.src('./public', {read: false})
+    .pipe(rimraf());
+});
+
+gulp.task('copy', function () {
+  gulp.src('./src/**/*.html')
+    .pipe(gulp.dest('./public'));
+});
+
+
+
+//TODO test, concat + minify js tasks
+gulp.task('test', ['clean', 'jshint', 'minify-html']);
