c73fb208c7e96405768f933120046ae0eeeec182
ifozest <aemaeth4@gmail.com>
2014-07-17 17:13:21 +0200

    added browserify task

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,7 @@ var gulp = require('gulp')
   , minifyHTML = require('gulp-minify-html')
   , rimraf = require('gulp-rimraf')
   , source = require('vinyl-source-stream')
-  , browserify = require('gulp-browserify')
+  , browserify = require('browserify')
   , plumber = require('gulp-plumber')
   , concat = require('gulp-concat')
   , jshint = require('gulp-jshint');
@@ -43,32 +43,16 @@ gulp.task('copy', function () {
     .pipe(gulp.dest('./public'));
 });
 
-gulp.task('scripts', function() {
-  //single entry point to browserify
-  gulp.src('./src/js/app.js')
-    .pipe(plumber())
-    .pipe(browserify({
-      shim: {
-        jquery : {
-          path: './vendor/jquery/dist/jquery.js',
-          exports: '$'
-        },
-        underscore: {
-          path: './vendor/underscore/underscore.js',
-          exports: '_'
-        },
-        Backbone: {
-          path: './vendor/backbone/backbone.js',
-          exports: 'Backbone'
-        },
-        Marionette: {
-          path: './vendor/backbone.marionette/lib/backbone.marionette.js',
-          exports: 'Marionette'
-        }
-      }
-    }))
-//    .pipe(concat('app.js'))
-    .pipe(gulp.dest('./public'))
+gulp.task('browserify', function() {
+  var b = browserify('./src/js/app.js');
+
+  return b.bundle()
+      .on('error', function (err) {
+        console.log(err.toString());
+        this.emit('end');
+      })
+      .pipe(source('app.js'))
+      .pipe(gulp.dest('./public/'));
 });
 
 gulp.task('watch', function () {
