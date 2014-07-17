d659bd436659732bc0ecd2d2f46eadc789c5f70a
ifozest <aemaeth4@gmail.com>
2014-07-17 21:20:07 +0200

    stub test files for infrastructure testing

---
+++ src/js/app.js
@@ -0,0 +1,5 @@
+var Backbone = require('backbone');
+
+var Handlebars = require("handlebars");
+console.log(require('handlebars'));
+var c = require('../templates/a.js');
---
+++ src/templates/another/template.js
@@ -0,0 +1,17 @@
+var Handlebars = require("handlebars");module.exports = Handlebars.template(function (Handlebars,depth0,helpers,partials,data) {
+  this.compilerInfo = [4,'>= 1.0.0'];
+helpers = this.merge(helpers, Handlebars.helpers); data = data || {};
+  var buffer = "", stack1, helper, functionType="function", escapeExpression=this.escapeExpression;
+
+
+  buffer += "<div class=\"entry\">\r\n  <h1>";
+  if (helper = helpers.title) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.title); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "</h1>\r\n  <div class=\"body\">\r\n    ";
+  if (helper = helpers.body) { stack1 = helper.call(depth0, {hash:{},data:data}); }
+  else { helper = (depth0 && depth0.body); stack1 = typeof helper === functionType ? helper.call(depth0, {hash:{},data:data}) : helper; }
+  buffer += escapeExpression(stack1)
+    + "\r\n  </div>\r\n</div>";
+  return buffer;
+  });
